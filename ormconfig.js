2e31619eaea6af716ce37b9976b4be71faf971c4
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-02 21:02:31 -0300

    add typeorm

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  "type": "mysql",
+  "host": process.env.DB_HOST,
+  "port": process.env.DB_PORT,
+  "database": process.env.DB_DATABASE,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "entities": ["dist/**/*.entity{.ts,.js}"],
+  "synchronize": true
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [],
 })
