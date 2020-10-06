6543f7fc595a6ba067e5091c3f71ff6ab25fac48
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-06 19:12:37 -0300

    fix linting errors

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-import * as session from 'express-session'
-import { sessionSettings } from './../src/session';
+import * as session from 'express-session';
+import { AppModule } from '../src/app.module';
+import { sessionSettings } from '../src/session';
 
 describe('App (e2e)', () => {
   let app: INestApplication;
@@ -14,7 +14,7 @@ describe('App (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.use(session(sessionSettings))
+    app.use(session(sessionSettings));
     await app.init();
   });
 
@@ -23,10 +23,7 @@ describe('App (e2e)', () => {
   });
 
   it('pong', () => {
-    return request(app.getHttpServer())
-      .get('/ping')
-      .expect(200)
-      .expect('pong');
+    return request(app.getHttpServer()).get('/ping').expect(200).expect('pong');
   });
 
   describe('RepoModule', () => {
@@ -34,58 +31,59 @@ describe('App (e2e)', () => {
 
     beforeAll(async () => {
       agent = request.agent(app.getHttpServer());
-    })
+    });
 
     it('should return bas request for repo not found', async () => {
-      const result = await agent.get("/repos/search/sdvonssdcnosvksnpsidn")
+      const result = await agent.get('/repos/search/sdvonssdcnosvksnpsidn');
 
-      expect(result.status).toEqual(400)
-    })
+      expect(result.status).toEqual(400);
+    });
 
     it('should return github (react) repository', async () => {
-      const result = await agent.get("/repos/search/react")
+      const result = await agent.get('/repos/search/react');
 
       expect(result.body).toMatchObject({
-        "githubId": 10270250,
-        "name": "react",
-      })
-    })
+        githubId: 10270250,
+        name: 'react',
+      });
+    });
 
     it('should show react repo', async () => {
-      const result = await agent.get("/repos")
+      const result = await agent.get('/repos');
 
-      expect(result.body).toMatchObject([{
-        "githubId": 10270250,
-        "name": "react",
-      }])
-    })
+      expect(result.body).toMatchObject([
+        {
+          githubId: 10270250,
+          name: 'react',
+        },
+      ]);
+    });
 
     it('should return github react and vue repository', async () => {
       // add vue to repos
-      await agent.get("/repos/search/vue")
+      await agent.get('/repos/search/vue');
 
-      const result = await agent.get("/repos")
-
-      expect(result.body).toContainEqual(
-        expect.objectContaining({
-          "githubId": 10270250,
-          "name": "react",
-        })
-      )
+      const result = await agent.get('/repos');
 
       expect(result.body).toContainEqual(
         expect.objectContaining({
-          "githubId": 11730342,
-          "name": "vue",
-        })
-      )
-    })
+          githubId: 10270250,
+          name: 'react',
+        }),
+      );
+
+      expect(result.body).toContainEqual(
+        expect.objectContaining({
+          githubId: 11730342,
+          name: 'vue',
+        }),
+      );
+    });
 
     it('empty session should show empty array', async () => {
-      const result = await request(app.getHttpServer()).get("/repos")
+      const result = await request(app.getHttpServer()).get('/repos');
 
-      expect(result.body).toEqual([])
-    })
-
-  })
+      expect(result.body).toEqual([]);
+    });
+  });
 });
