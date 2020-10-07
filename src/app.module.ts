739bd359a69b7dc0a3d7dabb459bf5f54fae4323
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-07 13:03:11 -0300

    add ScheduleModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { RepoModule } from './repo/repo.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), RepoModule],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), ScheduleModule.forRoot(), RepoModule],
   controllers: [AppController],
   providers: [],
 })
